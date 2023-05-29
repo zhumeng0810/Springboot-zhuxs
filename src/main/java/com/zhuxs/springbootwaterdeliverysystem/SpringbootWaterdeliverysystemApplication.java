7849094556df6da49c2e2af44c7d89a22e5b579b
zhumeng0810 <zhumeng0810@126.com>
2023-05-29 14:36:31 +0800

    初始创建

---
+++ src/main/java/com/zhuxs/springbootwaterdeliverysystem/SpringbootWaterdeliverysystemApplication.java
@@ -0,0 +1,13 @@
+package com.zhuxs.springbootwaterdeliverysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWaterdeliverysystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWaterdeliverysystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhuxs/springbootwaterdeliverysystem/SpringbootWaterdeliverysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuxs.springbootwaterdeliverysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWaterdeliverysystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
